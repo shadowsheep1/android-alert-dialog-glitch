ca43de1e6f8431d25c58d2a0082d5730b3dab55f
shadowsheep1 <16268789+shadowsheep1@users.noreply.github.com>
2021-07-01 14:12:32 +0200

    Add alert dialog management

--- app/src/main/java/com/example/alertdialogglitch/MainActivity.kt
+++ app/src/main/java/com/example/alertdialogglitch/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.alertdialogglitch
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,12 +11,41 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.example.alertdialogglitch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var inflater: LayoutInflater
+    private lateinit var dialogView: View
+    private lateinit var messageTextView: TextView
+    private val progressHud: AlertDialog by lazy {
+        val dialogBuilder =
+            AlertDialog.Builder(this, R.style.ProgressHudTheme)
+
+        messageTextView.visibility = View.GONE
+        dialogBuilder.setView(dialogView)
+        dialogBuilder.setCancelable(false)
+        dialogBuilder.create()
+    }
+
+    fun showProgressHud(message: String?) {
+        messageTextView.visibility = View.GONE
+        message?.let {
+            messageTextView.text = it
+            messageTextView.visibility = View.VISIBLE
+        }
+        progressHud.show()
+    }
+
+    fun hideProgressHud() {
+        progressHud.hide()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +63,14 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        setupProgressHud()
+    }
+
+    private fun setupProgressHud() {
+        inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        dialogView = inflater.inflate(R.layout.progress_hud, null)
+        messageTextView = dialogView.findViewById(R.id.progressHudTextView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
